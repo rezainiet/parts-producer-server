a97cc8e240fe9bae93fa677dfd9f27c8a831b8ef
rezainiet <masudrezaog@gmail.com>
2022-05-23 17:02:29 +0600

    project initialized

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 4000;
+
+//  middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(port, () => {
+    console.log(`app is running on port ${port}`);
+});
