48f3480757eaf5ec331bc9ffd85e6c65eb2a56f7
rezainiet <masudrezaog@gmail.com>
2022-05-25 05:47:11 +0600

    created post api for store order in database

--- index.js
+++ index.js
@@ -2,24 +2,53 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
+require('dotenv').config()
 
-//  middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.78gpd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('parts-producer').collection('products');
+        const orderCollection = client.db('parts-producer').collection('orders');
 
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = await productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
+        app.get('/product/:id', async (req, res) => {
+            const productId = req.params.id;
+            const query = { _id: ObjectId(productId) };
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        });
 
+        app.post('/order', async (req, res) => {
+            const product = req.body;
+            const result = await orderCollection.insertOne(product);
+            res.send(result);
+        })
+    }
+    finally {
 
-
-
+    }
+};
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Server is running');
+    res.send('app is running');
 });
 
 app.listen(port, () => {
-    console.log(`app is running on port ${port}`);
-});
+    console.log('app is running on server 4000')
+})
