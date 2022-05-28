847c739d138c946ee894b4c1e7a1ea8984e00c5e
rezainiet <masudrezaog@gmail.com>
2022-05-28 21:59:27 +0600

    created an api to make an admin

--- index.js
+++ index.js
@@ -91,6 +91,25 @@ async function run() {
             res.send(order);
         });
 
+
+        app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -102,6 +121,7 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc, options);
             res.send(result);
         });
+
 
         app.get('/user/:email', async (req, res) => {
             const email = req.params.email;
