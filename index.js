3ea520937576da1e543767bc3fc368da4bc698f0
rezainiet <masudrezaog@gmail.com>
2022-05-29 03:39:26 +0600

    created an api to delete a specific product

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(filter);
+            res.send(result);
+        })
+
         app.get('/product/:id', async (req, res) => {
             const productId = req.params.id;
             const query = { _id: ObjectId(productId) };
